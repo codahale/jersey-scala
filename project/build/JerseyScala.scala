cbb3311181236af2be272ca45484c39e58afc332
Coda Hale <coda.hale@gmail.com>
2010-12-02 17:36:21 -0800

    Move to Scala 2.8.1, sbt 0.7.5.RC0.

--- project/build/JerseyScala.scala
+++ project/build/JerseyScala.scala
@@ -1,4 +1,4 @@
-class JerseyScala(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with posterous.Publish with rsync.RsyncPublishing {
+class JerseyScala(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with posterous.Publish with rsync.RsyncPublishing with IdeaProject {
   /**
    * Publish the source as well as the class files.
    */
@@ -16,7 +16,7 @@ class JerseyScala(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with p
    */
   val sunRepo = "Sun Repo" at "http://download.java.net/maven/2/"
   
-  val liftJson = "net.liftweb" %% "lift-json" % "2.2-M1" withSources() intransitive()
+  val liftJson = "net.liftweb" % "lift-json_2.8.0" % "2.2-M1" withSources() intransitive()
   val paranamer = "com.thoughtworks.paranamer" % "paranamer" % "2.2.1" withSources() intransitive()
 
   val jaxrs = "javax.ws.rs" % "jsr311-api" % "1.1.1" withSources() intransitive()
@@ -24,7 +24,7 @@ class JerseyScala(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with p
   val jerseyServer = "com.sun.jersey" % "jersey-server" % "1.3" withSources() intransitive()
 
   val codasRepo = "Coda's Repo" at "http://repo.codahale.com"
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources()
-  val simplespec = "com.codahale" %% "simplespec" % "0.2.0-SNAPSHOT" % "test" withSources()
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test" withSources()
+  val simplespec = "com.codahale" %% "simplespec" % "0.2.0" % "test" withSources()
   val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
 }
