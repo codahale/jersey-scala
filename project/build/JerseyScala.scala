ad6121ac4f241db4a1311b2a92d95eb15407c49b
Coda Hale <coda.hale@gmail.com>
2011-01-07 23:47:00 -0800

    Move to maven-sbt, jerkson 0.1.2, jersey 1.5.0-ea08.

--- project/build/JerseyScala.scala
+++ project/build/JerseyScala.scala
@@ -1,6 +1,8 @@
 import sbt._
 
-class JerseyScala(info: ProjectInfo) extends DefaultProject(info) with posterous.Publish with IdeaProject {
+class JerseyScala(info: ProjectInfo) extends DefaultProject(info)
+                                             with IdeaProject
+                                             with maven.MavenDependencies {
   /**
    * Publish the source as well as the class files.
    */
@@ -8,30 +10,25 @@ class JerseyScala(info: ProjectInfo) extends DefaultProject(info) with posterous
   val sourceArtifact = Artifact.sources(artifactID)
   override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
 
-  /**
-   * Publish via Ivy.
-   */
   lazy val publishTo = Resolver.sftp("Personal Repo",
                                      "codahale.com",
-                                     "/home/codahale/repo.codahale.com/") as ("codahale")
-  override def managedStyle = ManagedStyle.Maven
+                                     "/home/codahale/repo.codahale.com/")
+
+  /**
+   * Repositories
+   */
+  val sunRepo = "Sun Repo" at "http://download.java.net/maven/2/"
+  val codasRepo = "Coda's Repo" at "http://repo.codahale.com"
 
   /**
    * Dependencies
    */
-  val sunRepo = "Sun Repo" at "http://download.java.net/maven/2/"
+  val jerkson = "com.codahale" %% "jerkson" % "0.1.2"
+  val jerseyServer = "com.sun.jersey" % "jersey-server" % "1.5-ea08"
 
-  val jerkson = "com.codahale" %% "jerkson" % "0.1.0" withSources () intransitive ()
-  val jacksonVersion = "1.6.2"
-  val jacksonCore = "org.codehaus.jackson" % "jackson-core-asl" % jacksonVersion withSources () intransitive ()
-  val jacksonMapper = "org.codehaus.jackson" % "jackson-mapper-asl" % jacksonVersion withSources () intransitive ()
-  val paranamer = "com.thoughtworks.paranamer" % "paranamer" % "2.3" withSources () intransitive ()
-
-  val jerseyCore = "com.sun.jersey" % "jersey-core" % "1.5-ea06" withSources() intransitive()
-  val jerseyServer = "com.sun.jersey" % "jersey-server" % "1.5-ea06" withSources() intransitive()
-
-  val codasRepo = "Coda's Repo" at "http://repo.codahale.com"
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test" withSources()
-  val simplespec = "com.codahale" %% "simplespec" % "0.2.0" % "test" withSources()
-  val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
+  /**
+   * Test Dependencies
+   */
+  val simplespec = "com.codahale" %% "simplespec" % "0.2.0" % "test"
+  val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test"
 }
