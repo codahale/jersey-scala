b78a3c639b99d436230a6e64dbf88a6022e9cc31
Coda Hale <coda.hale@gmail.com>
2011-03-29 15:55:55 -0700

    Upgrade to Jersey 1.6 and Jerkson 0.1.6.

--- project/build/JerseyScalaProject.scala
+++ project/build/JerseyScalaProject.scala
@@ -23,8 +23,8 @@ class JerseyScalaProject(info: ProjectInfo) extends DefaultProject(info)
   /**
    * Dependencies
    */
-  val jerkson = "com.codahale" %% "jerkson" % "0.1.4"
-  val jerseyServer = "com.sun.jersey" % "jersey-server" % "1.5"
+  val jerkson = "com.codahale" %% "jerkson" % "0.1.6"
+  val jerseyServer = "com.sun.jersey" % "jersey-server" % "1.6"
   val slf4j = "org.slf4j" % "slf4j-api" % "1.6.1"
 
   /**
